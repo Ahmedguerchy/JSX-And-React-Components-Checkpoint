56f3302c9b2196571cfd05348cafd09a9294df6b
Ahmedguerchy <79759942+Ahmedguerchy@users.noreply.github.com>
2022-08-24 17:13:41 +0100

    Add files via upload

---
+++ src/components/Profile/FullName.js
@@ -0,0 +1,9 @@
+import React from "react";
+const FullName=()=>{
+    return(
+        <div>
+            <h2 className="Name">Ahmed Guerchy</h2>
+        </div>
+    )
+}
+export default FullName;
---
+++ src/components/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css'
+const ProfilePhoto=()=>{
+    return(
+        <div >
+            <img  className="Photo" src ='/AG.jpg'/>
+        </div>
+    )
+}
+export default ProfilePhoto;
